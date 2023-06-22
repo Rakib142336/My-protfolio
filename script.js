4d953ac3fa3ec5d121268b1600c769e78d59fa6b
Rakib Hasan <125240662+Rakib142336@users.noreply.github.com>
2023-06-22 21:11:06 +0600

    Cybar Path
    Figma to HTML,CSS,Javascript using code convert

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// header bar
+//mobile menu js
 let menu = document.querySelector(".menuIcon")
 let mobileMenu = document.querySelector(".mobile__menu")
 const xMark = document.querySelector(".crossMark")
@@ -11,3 +11,70 @@ menu.addEventListener('click', function(){
 xMark.addEventListener('click', function(){
     mobileMenu.classList.remove("show")
 })
+// 
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  margin:2,
+  nav:true,
+  autoplay:true,
+  autoplayTimeout:1000,
+  responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:4
+      },
+      1000:{
+          items:4
+      }
+  }
+})
+
+//  slide__section
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:2,
+    nav:true,
+    autoplay:true,
+    // autoplayTimeout:1000,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:4
+        },
+        1000:{
+            items:4
+        }
+    }
+})
+
+// =======
+var swiper = new Swiper(".mySwiper", {
+  loop: true,
+  autoplay: true,
+    slidesPerView: 1,
+    spaceBetween:10,
+    pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    },
+   
+    breakpoints: {
+      "@0.00": {
+        slidesPerView: 1,
+        spaceBetween: 10,
+      },
+      "@0.75": {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      "@1.00": {
+        slidesPerView: 3,
+        spaceBetween: 40,
+      },
+      
+    },
+  });
